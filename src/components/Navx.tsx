59b8d106151d55037d6235efb173d1b45539c3bf
Lakara2 <lbryanlakara@gmail.com>
2024-01-24 10:02:48 +0300

    feat: update nav

--- src/components/Navx.tsx
+++ src/components/Navx.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import '../style/style.css';
+import SignCraft from '../assets/SignCraft.png';
 
 const Navx: React.FC = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -9,18 +11,20 @@ const Navx: React.FC = () => {
 
   return (
     <nav
-      className="navbar navbar-expand-md navbar-dark bg-dark"
+      className="navbar navbar-expand-md navbar-dark bg-sign"
       style={{ padding: '20px' }}
     >
       <a className="navbar-brand" href="/">
-        Mon Logo
+        <img className="singCraft" src={SignCraft} alt="" />
       </a>
       <button className="navbar-toggler" type="button" onClick={toggleNav}>
         <span className="navbar-toggler-icon"></span>
       </button>
+        <h1 className='text-sign'>SIGN CRAFT</h1>
       <div
         className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''} justify-content-end`}
       >
+        {/*
         <div className="form-inline ml-auto">
           <input
             className="form-control mr-sm-2"
@@ -28,27 +32,25 @@ const Navx: React.FC = () => {
             placeholder="Recherche"
             aria-label="Recherche"
           />
-        </div>
-        <ul className="navbar-nav">
+        </div>*/}
+        <ul className="navbar-nav text-dark marg-sign">
           <li className="nav-item">
-            <a className="nav-link" href="/">
+            <a className="nav-link text-dark" href="/">
               Accueil
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/about">
+            <a className="nav-link text-dark" href="/about">
               À propos
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/contact">
+            <a className="nav-link text-dark" href="/contact">
               Contact
             </a>
           </li>
+          <input type="button" className="btn bg-danger" value="logout" />
         </ul>
-      </div>
-      <div>
-        <input type="button" className='btn bg-danger' value="logout" />
       </div>
     </nav>
   );
