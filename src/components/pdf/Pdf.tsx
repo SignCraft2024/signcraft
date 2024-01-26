8671934bb8e2e57e8c96b4e2d2d7c30b51e14174
Micael Jerry <hei.jerry.2@gmail.com>
2024-01-26 12:00:10 +0300

    bug: implement AddSignDialog

--- src/components/pdf/Pdf.tsx
+++ src/components/pdf/Pdf.tsx
@@ -5,6 +5,7 @@ import { Drop } from './Drop';
 import { blobToURL } from '../../utils/Utils';
 import { Document, Page, pdfjs } from 'react-pdf';
 import { PagingControl } from './PagingControl/PagingControl';
+import { AddSignDialog } from './AddSignDialog/AddSignDialog';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -14,6 +15,10 @@ const Pdf = () => {
 	const [pageDetails, setPageDetails] = useState(null);
 	const [pageNum, setPageNum] = useState<number>(0);
 	const documentRef = useRef(null);
+	const [signatureDialogVisible, setSignatureDialogVisible] =
+		useState<boolean>(false);
+	const [autoDate, setAutoDate] = useState<boolean>(true);
+	const [signatureURL, setSignatureURL] = useState<string | null>(null);
 
 	const onLoaded = async (files: Blob[]) => {
 		const URL: string | null = await blobToURL(files[0]);
@@ -24,6 +29,17 @@ const Pdf = () => {
 		<div>
 			<Header />
 			<div className="container">
+				{signatureDialogVisible ? (
+					<AddSignDialog
+						autoDate={autoDate}
+						setAutoDate={setAutoDate}
+						onClose={() => setSignatureDialogVisible(false)}
+						onConfirm={(url) => {
+							setSignatureURL(url);
+							setSignatureDialogVisible(false);
+						}}
+					/>
+				) : null}
 				{!pdf ? <Drop onLoaded={onLoaded} /> : null}
 				{pdf ? (
 					<div>
@@ -53,6 +69,6 @@ const Pdf = () => {
 			</div>
 		</div>
 	);
-}
+};
 
 export default Pdf;
