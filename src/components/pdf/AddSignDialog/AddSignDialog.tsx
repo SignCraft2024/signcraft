90c58f5570ff5e90d4cb2d538aaa19b5ad9dcb8e
Micael Jerry <hei.jerry.2@gmail.com>
2024-01-28 15:32:46 +0300

    style: run prettify

--- src/components/pdf/AddSignDialog/AddSignDialog.tsx
+++ src/components/pdf/AddSignDialog/AddSignDialog.tsx
@@ -1,15 +1,18 @@
-import './AddSignDialog.css'
-import React, { useRef } from 'react'
+import './AddSignDialog.css';
+import React, { useRef } from 'react';
 
 type AddSignDialogProps = {
-  autoDate: boolean;
-  setAutoDate: (params: any) => void;
-  onClose: () => void;
-  onConfirm: (url: string) => void;
-}
-export const AddSignDialog: React.FC<AddSignDialogProps> = ({autoDate, setAutoDate, onClose, onConfirm}) => {
-  const signRef = useRef(null);
-  return (
-    <div></div>
-  )
+	autoDate: boolean;
+	setAutoDate: (params: any) => void;
+	onClose: () => void;
+	onConfirm: (url: string) => void;
+};
+export const AddSignDialog: React.FC<AddSignDialogProps> = ({
+	autoDate,
+	setAutoDate,
+	onClose,
+	onConfirm,
+}) => {
+	const signRef = useRef(null);
+	return <div></div>;
 };
