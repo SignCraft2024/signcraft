9dd73f445fadf19d0d13916f0cee45cb131aae26
tahiry-dev <tahirycontact@gmail.com>
2024-01-30 19:25:42 +0300

    fix:update app.tsx for solve conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css';
 import NavBar from './components/Navx';
 import './styles/nav.css';
 import './App.css'
 import Footx from './components/Footx';
+import Pdf from './components/pdf/Pdf';
 import ShowFile from './component/file_upload/ShowFile';
 import './components/ProfilePage';
 import './styles/profile.css';
-
 
 
 function App() {
@@ -16,6 +15,7 @@ function App() {
       <NavBar>
       <ProfilePage/>
       <ShowFile/>
+        <Pdf />
     </>
   );
 }
---
+++ src/components/pdf/header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+	return (
+		<div className="container">
+			<div>Open PDF Sign</div>
+		</div>
+	);
+}
+
+export default Header;
