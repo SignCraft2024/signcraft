c0cef179269253e35a6228e4e49083fe5fafe8b1
tahiry-dev <tahirycontact@gmail.com>
2024-01-30 19:14:48 +0300

    fix: temporary fix of conflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import './App.css';
 import NavBar from './components/Navx';
 import './styles/nav.css';
 import './App.css'
-import Footx from './components/Footx'
-import './components/ProfilePage'
+import Footx from './components/Footx';
+import ShowFile from './component/file_upload/ShowFile';
+import './components/ProfilePage';
 import './styles/profile.css';
 
 
 
 function App() {
 
-
   return (
     <>
-      
     <NavBar>
     <ProfilePage/>
-
+      <ShowFile/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
