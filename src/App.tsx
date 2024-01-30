b33a2df1a3cee4828c7f4f4d203a1b0260af4207
FaniryKeziah <hei.keziah.2@gmail.com>
2024-01-30 13:32:30 +0300

    feat: add routes for login and signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 /*import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'*/
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Log from './components/login/Login'
+import Signin from './components/signin/Sign'
 
 function App() {
   /*const [count, setCount] = useState(0)*/
@@ -8,7 +11,12 @@ function App() {
 
   return (
     <>
-     Hello world
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Log/>} />
+          <Route path="/signin" element={<Signin/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
