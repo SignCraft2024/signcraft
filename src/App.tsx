dab1aa9444f901b958396db68877e0fae8c94e62
NiceGuyMe <boy-unfind@proton.me>
2024-02-02 15:19:05 +0300

    fusion login && signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-/*import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'*/
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Log from './components/login/Login'
 import Signin from './components/signin/Sign'
 
 function App() {
-  /*const [count, setCount] = useState(0)*/
-
 
   return (
     <>
