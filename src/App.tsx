e83040a95dcc4ecdbd057808ac6d7b66789b0534
NiceGuyMe <boy-unfind@proton.me>
2024-02-02 16:54:39 +0300

    fix conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import './App.css';
-import NavBar from './components/Navx';
-import './styles/nav.css';
-import './App.css'
-import Footx from './components/Footx';
+import './styles/nav.css'
 import ShowFile from './component/file_upload/ShowFile';
-import './components/ProfilePage';
 import './styles/profile.css';
 
 
 
 function App() {
-
   return (
     <>
-    <NavBar>
-    <ProfilePage/>
       <ShowFile/>
     </>
-  );
+  )
 }
 
 export default App;
--- src/component/file_upload/ShowFile.tsx
+++ src/component/file_upload/ShowFile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import FormUpload from './FormUpload';
 
 export default function ShowFile() {
