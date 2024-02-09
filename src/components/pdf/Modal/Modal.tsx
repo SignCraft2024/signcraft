b3a4fb8b90372c08cec49974504813c65bb87825
Micael Jerry <hei.jerry.2@gmail.com>
2024-02-09 11:21:34 +0300

    bug style modal

--- src/components/pdf/Modal/Modal.tsx
+++ src/components/pdf/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-// import { useIsSmallScreen } from '../../../hooks/useIsSmallScreen';
+import { useIsSmallScreen } from '../../../hooks/useIsSmallScreen';
+import './Modal.css'
 
 type ModalProps = {
 	onClose: () => void;
@@ -13,40 +14,34 @@ export const Modal: React.FC<ModalProps> = ({
 	onClose,
 	children,
 	isVisible,
-	// style,
-	// positionTop,
+	style,
+	positionTop,
 }) => {
-	// const isSmallScreen: boolean = useIsSmallScreen();
+	const isSmallScreen: boolean = useIsSmallScreen();
 
-	// const styles = {
-	// 	container: {
-	// 		position: isSmallScreen ? 'fixed' : 'absolute',
-	// 		backgroundColor: '#FFF',
-	// 		border: `1px solid hsl(218,49%,66%)`,
-	// 		borderRadius: 4,
-	// 		top: positionTop ?? (isSmallScreen ? 60 : 150),
-	// 		left: '50%',
-	// 		transform: 'translateX(-50%)',
-	// 		width: '94%',
-	// 		fontFamily: 'Open Sans',
-	// 		zIndex: 10000,
-	// 		boxShadow: '0 0px 14px hsla(0, 0%, 0%, 0.2)',
-	// 	},
-	// 	background: {
-	// 		position: 'fixed',
-	// 		width: '100%',
-	// 		height: '100%',
-	// 		top: 0,
-	// 		left: 0,
-	// 		backgroundColor: '#00000033',
-	// 		zIndex: 5000,
-	// 	},
-	// };
+	const styles = {
+		container: {
+			position: isSmallScreen ? 'fixed' : 'absolute',
+			borderRadius: 4,
+			top: positionTop ?? (isSmallScreen ? 60 : 150),
+			left: '50%',
+			transform: 'translateX(-50%)',
+			width: '94%',
+			fontFamily: 'Open Sans',
+			zIndex: 10000,
+			boxShadow: '0 0px 14px hsla(0, 0%, 0%, 0.2)',
+		}
+	};
 
 	return isVisible ? (
 		<div>
-			{/* <div style={styles.background} onClick={onClose} /> */}
-			{/* <div style={{ ...styles.container, ...style }}>{children}</div> */}
+			<div className="modal-background" onClick={onClose} />
+			<div
+				className="modal-container"
+				style={{ ...styles.container, ...style }}
+			>
+				{children}
+			</div>
 			<div onClick={onClose} />
 			<div>{children}</div>
 		</div>
