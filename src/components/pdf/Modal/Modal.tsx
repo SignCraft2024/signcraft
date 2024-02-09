1d480ec29da974ff6f5317f6d772301307b929ed
Micael Jerry <hei.jerry.2@gmail.com>
2024-02-09 17:33:52 +0300

    fix: signature invisible

--- src/components/pdf/Modal/Modal.tsx
+++ src/components/pdf/Modal/Modal.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useIsSmallScreen } from '../../../hooks/useIsSmallScreen';
-import './Modal.css';
+// import { useIsSmallScreen } from '../../../hooks/useIsSmallScreen';
 
 type ModalProps = {
 	onClose: () => void;
@@ -13,24 +12,79 @@ export const Modal: React.FC<ModalProps> = ({
 	onClose,
 	children,
 	isVisible,
-	positionTop,
+	// style,
+	// positionTop,
 }) => {
-	const isSmallScreen: boolean = useIsSmallScreen();
+	// const isSmallScreen: boolean = useIsSmallScreen();
+
+	// const styles = {
+	// 	container: {
+	// 		position: isSmallScreen ? 'fixed' : 'absolute',
+	// 		backgroundColor: '#FFF',
+	// 		border: `1px solid hsl(218,49%,66%)`,
+	// 		borderRadius: 4,
+	// 		top: positionTop ?? (isSmallScreen ? 60 : 150),
+	// 		left: '50%',
+	// 		transform: 'translateX(-50%)',
+	// 		width: '94%',
+	// 		fontFamily: 'Open Sans',
+	// 		zIndex: 10000,
+	// 		boxShadow: '0 0px 14px hsla(0, 0%, 0%, 0.2)',
+	// 	},
+	// 	background: {
+	// 		position: 'fixed',
+	// 		width: '100%',
+	// 		height: '100%',
+	// 		top: 0,
+	// 		left: 0,
+	// 		backgroundColor: '#00000033',
+	// 		zIndex: 5000,
+	// 	},
+	// };
 
 	return isVisible ? (
 		<div>
-			<div className="modal-background" onClick={onClose} />
-			<div
-				className="modal-container"
-				style={{
-					position: isSmallScreen ? 'fixed' : 'absolute',
-					top: positionTop ?? (isSmallScreen ? 60 : 150),
-				}}
-			>
-				{children}
-			</div>
+			{/* <div style={styles.background} onClick={onClose} /> */}
+			{/* <div style={{ ...styles.container, ...style }}>{children}</div> */}
 			<div onClick={onClose} />
 			<div>{children}</div>
 		</div>
 	) : null;
 };
+
+// import React from 'react';
+// import { useIsSmallScreen } from '../../../hooks/useIsSmallScreen';
+// import './Modal.css';
+
+// type ModalProps = {
+// 	onClose: () => void;
+// 	children: React.ReactNode;
+// 	isVisible: boolean;
+// 	positionTop?: number;
+// };
+
+// export const Modal: React.FC<ModalProps> = ({
+// 	onClose,
+// 	children,
+// 	isVisible,
+// 	positionTop,
+// }) => {
+// 	const isSmallScreen: boolean = useIsSmallScreen();
+
+// 	return isVisible ? (
+// 		<div>
+// 			<div className="modal-background" onClick={onClose} />
+// 			<div
+// 				className="modal-container"
+// 				style={{
+// 					position: isSmallScreen ? 'fixed' : 'absolute',
+// 					top: positionTop ?? (isSmallScreen ? 60 : 150),
+// 				}}
+// 			>
+// 				{children}
+// 			</div>
+// 			<div onClick={onClose} />
+// 			<div>{children}</div>
+// 		</div>
+// 	) : null;
+// };
