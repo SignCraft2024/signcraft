afe743705da5356df8a4a38c726fbf3d7f77b2e6
Micael Jerry <hei.jerry.2@gmail.com>
2024-02-11 20:05:05 +0300

    fix: create Dialog

---
+++ src/components/pdf/mypdf/mypdfComponents/Dialog.tsx
@@ -0,0 +1,67 @@
+import {FaTimes} from 'react-icons/fa';
+import { primary45 } from '../../../../utils/colors';
+import { Modal } from './Modal';
+import React, { CSSProperties } from 'react';
+
+type DialogProps = {
+	isVisible: boolean;
+	body: React.ReactNode;
+	onClose: () => void;
+	title?: string;
+	noPadding?: boolean;
+	backgroundColor?: string;
+	positionTop?: number;
+  style: CSSProperties;
+};
+
+export const Dialog: React.FC<DialogProps> = ({
+  isVisible,
+  body,
+  onClose,
+  title,
+  noPadding,
+  backgroundColor,
+  positionTop,
+  style,
+}) => {
+  if (!isVisible) {
+    return null;
+  }
+
+  const styles = {
+    header: {
+      backgroundColor: primary45,
+      color: '#FFF',
+      padding: 8,
+      fontSize: 14,
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    body: {
+      padding: noPadding ? 0 : 14,
+      backgroundColor: backgroundColor ?? '#FFF',
+    },
+    xIcon: {
+      cursor: 'pointer',
+    },
+  };
+
+  return (
+    <Modal onClose={onClose} isVisible={isVisible} positionTop={positionTop} style={style}>
+      <div>
+        <div style={styles.header}>
+          <div>{title}</div>
+          <FaTimes
+            color={'#FFF'}
+            size={16}
+            style={styles.xIcon}
+            className={'dialogClose'}
+            onClick={onClose}
+          />
+        </div>
+        <div style={styles.body}>{body}</div>
+      </div>
+    </Modal>
+  );
+}
