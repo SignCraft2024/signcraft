4f14d73d630ba352f6d080ed2882c9df6bcd2f50
NiceGuyMe <boy-unfind@proton.me>
2024-02-14 11:20:27 +0300

    feat: upload file on firebase

--- src/components/pdf/Drop.tsx
+++ src/components/pdf/Drop.tsx
@@ -1,13 +1,26 @@
 import './Drop.css';
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { storage } from '../../firebase/firebase';
 
 export const Drop: React.FC<{ onLoaded: (files: Blob[]) => void }> = ({
 	onLoaded,
 }) => {
 	const onDrop = useCallback((acceptedFiles: File[]) => {
+		const storageRef = ref(storage, `/files/${acceptedFiles[0].name}`);
+		const uploadTask = uploadBytesResumable(storageRef, acceptedFiles[0]);
+		uploadTask.on(
+			'state_changed',
+			(err) => console.log(err),
+			() => {
+				// download url
+				getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+					console.log(url);
+				});
+			},
+		);
 		onLoaded(acceptedFiles);
-		// Do something with the files
 	}, []);
 	const { getRootProps, getInputProps, isDragActive } = useDropzone({
 		onDrop,
