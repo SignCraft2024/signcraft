5e9544018abc351c6ede53e3e24190aca74fdfe5
SoaMariaka19 <hei.soa.2@gmail.com>
2024-02-18 15:46:22 +0300

    feat: added error handling for login credentials and signup email duplicates

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -104,7 +104,6 @@ export const Auth: React.FC<AuthProps> = ({ isLogin }) => {
 							{formik.touched.password && formik.errors.password && (
 								<span className="error">{formik.errors.password}</span>
 							)}
-							<span>{error}</span>
 						</Stack>
 						<FormButton isLogin={isLogin} />
 					</Stack>
